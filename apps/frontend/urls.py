a3e2b82d6dfec25bb8728aab6aec1d77f34d3cc5
JanCas <jan.l.cas@outlook.com>
2020-12-22 09:12:22 -0800

    auth0

--- apps/frontend/urls.py
+++ apps/frontend/urls.py
@@ -9,11 +9,6 @@ from .views.ElectionInLineView import ElectionInLineViewSet
 from .views.VoterFavElectionsView import VoterFavElectionViewSet
 from .views.VoterView import VoterViewSet
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-
 router = routers.DefaultRouter()
 router.register(r'Candidate', CandidateViewSet)
 router.register(r'Election', ElectionViewSet)
@@ -26,7 +21,5 @@ router.register(r'Voter', VoterViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
