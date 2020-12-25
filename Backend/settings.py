aeab3b00d5c08733414584c4bdb58d861cfac6fd
JanCas <jan.l.cas@outlook.com>
2020-12-25 15:41:04 -0800

    Update settings.py

--- Backend/settings.py
+++ Backend/settings.py
@@ -120,7 +120,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Django Rest Permissions
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+#        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny'
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
