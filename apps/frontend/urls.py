e5f1a37a17b2240201239720ec11b377955f0737
janlc <jan.l.cas@outlook.com>
2020-12-30 20:02:09 -0800

    roolback to user

--- apps/frontend/urls.py
+++ apps/frontend/urls.py
@@ -8,6 +8,7 @@ from .views.ElectionInLineView import ElectionInLineViewSet
 from .views.VoterFavElectionsView import VoterFavElectionViewSet
 from .views.VoterView import VoterViewSet
 from .views.PoliticalPartyView import PoliticalPartyViewSet
+from .views.UserView import UserViewSet
 
 router = routers.DefaultRouter()
 router.register(r'Candidate', CandidateViewSet)
@@ -18,6 +19,7 @@ router.register(r'ElectionInLine', ElectionInLineViewSet)
 router.register(r'VoterFavElections', VoterFavElectionViewSet)
 router.register(r'Voter', VoterViewSet)
 router.register(r'PoliticalParty', PoliticalPartyViewSet)
+router.register(r'User', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
