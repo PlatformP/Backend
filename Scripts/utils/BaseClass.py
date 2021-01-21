a3fb40c4516a2636acff862ee100d20f3fccee8e
JanCas <jan.l.cas@outlook.com>
2021-01-21 11:01:34 -0700

    Base Class

---
+++ Scripts/utils/BaseClass.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Base(models.Model):
+
+    class Meta:
+        abstract = True
+
+    def update_with_kwargs(self, **kwargs):
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+        self.save()
